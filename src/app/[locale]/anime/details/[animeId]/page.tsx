b1ef036817468b076191e6c8bff17d96471c39de
Filipe Leoni <filipe_sempre@outlook.com>
2023-09-09 16:14:56 -0300

    fix: Component episode bug fixed

--- src/app/[locale]/anime/details/[animeId]/page.tsx
+++ src/app/[locale]/anime/details/[animeId]/page.tsx
@@ -1,29 +1,10 @@
-import DetailsBanner from "@/components/shared/DetailsBanner";
 import React from "react";
-
-async function getDetails(animeId: string) {
-  const res = await fetch(`https://graphql.anilist.co${name}`);
-  return res.json();
-}
-
-export async function generateStaticParams() {
-  const details = await fetch("https://graphql.anilist.co", {
-    method: "POST",
-  }).then((res) => res.json());
-  console.log(details);
-
-  return details.map((post: any) => ({
-    animeId: post.id,
-  }));
-}
 
 export default function page({ params }: { params: { animeId: string } }) {
   console.log(params);
   return (
     <>
-      <div className="pb-8">
-        {/* <DetailsBanner image={anime.bannerImage} /> */}
-      </div>
+      <div className="pb-8">Details page</div>
     </>
   );
 }
